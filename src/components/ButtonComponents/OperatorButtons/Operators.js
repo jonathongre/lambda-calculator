cdffbb2d9cbf0051f005a709d5b9d7c5ac23987f
Jonathon Green <ericg9811@gmail.com>
2019-07-16 10:49:48 -0500

    Export operators into Operators.js. Import Operators component into App.js

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { operators } from "../../../data";
 
 //import any components needed
 
@@ -6,6 +7,7 @@ import React from "react";
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+    console.log("operators", operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
@@ -14,3 +16,4 @@ const Operators = () => {
     </div>
   );
 };
+export default Operators;
